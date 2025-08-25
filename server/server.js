543bd1c8b85f37946932a47d0b3621488f0deb80
hemanthbanala <141825417+hemanthbanala@users.noreply.github.com>
2025-08-25 21:51:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,6 @@ app.use(bodyParser.json());
 app.use('/api/todos', todoRoutes);
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
